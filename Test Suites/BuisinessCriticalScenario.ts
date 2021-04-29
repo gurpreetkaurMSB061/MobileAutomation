51dfc20c337ffbaff421f287d17eac88a2940f98
Gurpreet Kaur <gurpreet.kaur@msbdocs.com>
2021-04-29 19:03:25 +0530

    WithObjectRepository

---
+++ Test Suites/BuisinessCriticalScenario.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>BuisinessCriticalScenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>531ec746-0ccd-45f0-8419-17cb2a18f8df</testSuiteGuid>
+   <testCaseLink>
+      <guid>99b57ee0-539f-4ed5-ba96-fe8c4005e759</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginPageManualTestCase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
